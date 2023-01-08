9c852a88d90a832826313a4974f585ac51a637aa
REJOINT <brecab120@gmail.com>
2023-01-08 14:40:14 -0500

    Updates

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,48 @@
 
 // Your Code Here
+async function main(){
+    let response = await fetch('http://localhost:3001/listBooks')
+
+    let books = await response.json()
+
+    console.log(books)
+
+    books.forEach(renderBook)
+
+}
+
+function renderBook(book) {
+    let root =document.querySelector('#root')
+
+    let li = document.createElement('li')
+    li.textContent = book.title
+
+    let quantityInput =document.createElement('input')
+    quantityInput.value = book.quantity
+
+    let saveButton = document.createElement('button')
+    saveButton.texContent = "Save"
+
+    saveButton.addEventListener('click', function(){
+        fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify({
+                id: book.id,
+                quantity: quantityInput.value
+
+            })
+            
+        })
+    })
+
+    li.append(quantityInput, saveButton)
+
+    root.append(li)
+
+
+}
+
+main()
